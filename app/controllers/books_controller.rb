e067e1e6de7d16c0ae7aaeb736c389b36fd8587f
Shinji <117142484+takenokonosato1969@users.noreply.github.com>
2023-04-11 07:12:56 +0000

    課題3 コントローラーを先に書いてcreateを試したい

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,38 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @user = User.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = User.new
   end
   
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
   end
   
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to user_path(@book.user_id)
+    else
+      render "/books"
+    end
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body ,:user_id)
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,45 @@
 class UsersController < ApplicationController
   def index
+    @user=User.new
+    @book=Book.new
   end
 
   def show
     @user = User.find(params[:id])
+    # @book = Book.find(params[:id])
   end
   
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to user_path(@book.user_id)
+    else
+      render "/books"
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :user_image)
+  end
+  
+  def is_matching_login_user
+    user = Use.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to sign_in_path
+    end
+  end
   
 end
