2aedbbd6b6655fbd25ba77083729fea97c3189a9
Shinji <117142484+takenokonosato1969@users.noreply.github.com>
2023-04-13 13:29:09 +0000

    課題3　150あったエラーが18に減った

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get '/homes/about', to: 'homes#about', as: 'about'
+  # get '/homes/about', to: 'homes#about', as: 'about'
+  get 'home/about' => 'homes#about', as: 'about'
 
   resources :users, only: [:index, :show, :edit, :update ]
   resources :books
